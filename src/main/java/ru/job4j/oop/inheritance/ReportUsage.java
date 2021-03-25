e6f7f16dc8982ec647ff7c01842c28679e6f26ca
NDreKru <ndrekrukru@gmail.com>
2021-03-25 19:17:30 +0500

    4. Переопределение [#483151]

---
+++ src/main/java/ru/job4j/oop/inheritance/ReportUsage.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop.inheritance;
+
+public class ReportUsage {
+
+    public static void main(String[] args) {
+        TextReport report = new TextReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
+        HtmlReport htmlReport = new HtmlReport();
+        String htmlText = htmlReport.generate("Report's name", "Report's body");
+        System.out.println(htmlText);
+        JSONReport jsonReport = new JSONReport();
+        String jsonText = jsonReport.generate("Report's name", "Report's body");
+        System.out.println(jsonText);
+    }
+}
