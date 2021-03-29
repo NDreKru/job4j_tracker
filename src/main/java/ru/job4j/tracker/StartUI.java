506e1e5fd26fd356c25acf29b0637e7052a5fb6d
NDreKru <ndrekrukru@gmail.com>
2021-03-29 20:26:29 +0500

    9. toString [#483166]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,10 +1,14 @@
 package ru.job4j.tracker;
 
+import java.util.Date;
+
 public class StartUI {
 
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        tracker.add(new Item("test item 1"));
-        System.out.println("Founded item name: " + tracker.findById(1).getName());
+//        Tracker tracker = new Tracker();
+//        tracker.add(new Item("test item 1"));
+//        System.out.println("Founded item name: " + tracker.findById(1).getName());
+        Item item = new Item(1, "First");
+        System.out.println(item);
     }
 }
