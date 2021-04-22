a88e0461912c3dcc45ef75fffef1063bb2da5d0b
ndre <ndrekrukru@gmail.com>
2021-04-22 07:35:48 +0500

    4. final [#483127]

--- src/main/java/ru/job4j/ooa/Airport.java
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -2,15 +2,15 @@ package ru.job4j.ooa;
 
 public class Airport {
     public static void main(String[] args) {
-        final Airbus AIRBUS = new Airbus("A320");
-        System.out.println(AIRBUS);
-        AIRBUS.printModel();
-        AIRBUS.printCountEngine();
+        final Airbus airbus = new Airbus("A320");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
 
         Airbus airbusA380 = new Airbus("A380");
         System.out.println(airbusA380);
 
-        AIRBUS.setName("A380");
-        System.out.println(AIRBUS);
+        airbus.setName("A380");
+        System.out.println(airbus);
     }
 }
