b2ff09ab7f3e026c3b1b9cf60e60ddabf1adc18b
NDreKru <ndrekrukru@gmail.com>
2021-04-25 21:10:00 +0500

    1. Телефонный справочник на базе ArrayList [#483066]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -18,4 +18,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
+
+    @Test
+    public void whenNotFound() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("abcd");
+        assertTrue(persons.isEmpty());
+    }
 }
